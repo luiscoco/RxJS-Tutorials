2054349fbcccb18a759ff3b134a920b6963394b6
neal.yip <neal.yip@webssup.com>
2018-05-03 16:29:40 +0800

    add output to finally

--- finally.js
+++ finally.js
@@ -4,9 +4,17 @@ require('rxjs/add/observable/of');
 require('rxjs/add/operator/finally');
 
 Observable.throw(Error('a'))
-    .finally(() => console.log('run'))
+    .finally(() => console.log('run a'))
     .subscribe(void 0, e => console.error(e.message));
 
 Observable.of('b')
-    .finally(() => console.log('run'))
+    .finally(() => console.log('run b'))
     .subscribe(result => console.error(result));
+
+/**
+ output
+ a
+ b
+ run a
+ run b
+ **/
