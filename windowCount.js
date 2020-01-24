1926043ccc5456da7a68d807daf9e09b019dc247
neal.yip <neal.yip@codpayment.com>
2020-01-24 10:59:11 +0800

    Add some explanation

--- windowCount.js
+++ windowCount.js
@@ -7,13 +7,17 @@ require('rxjs/add/operator/windowTime');
 const windowCount = require('rxjs/operators').windowCount;
 const tap        = require('rxjs/operators').tap;
 const mergeAll   = require('rxjs/operators').mergeAll;
+const flatMap   = require('rxjs/operators').flatMap;
+const of   = require('rxjs/observable/of').of;
 
 
 const source = Observable.timer(0, 1000);
 
 source.pipe(
     windowCount(3),
-    tap(() => console.log('hello'))
+    tap(() => console.log('hello')),
+    // tap(subject => console.log(subject)), // we can exploit it to do something regularly after the event in every N times
+    // for instance, calculating the current progress
 ).pipe(mergeAll())
     .subscribe(console.log);
 
