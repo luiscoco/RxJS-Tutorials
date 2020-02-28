8b49865663f7b754993ea71ce2609be2b00a5944
faiyip pc <faiyip@gmail.com>
2020-02-28 15:16:14 +0800

    Added some examples to throttle

--- throttle.js
+++ throttle.js
@@ -2,7 +2,12 @@
 
 const Observable = require('rxjs/Observable').Observable;
 require('rxjs/add/operator/throttle');
-const timer = require('rxjs/observable/timer').timer;
+require('rxjs/add/observable/of');
+require('rxjs/add/observable/never');
+const timer    = require('rxjs/observable/timer').timer;
+const throttle = require('rxjs/operators').throttle;
+const tap      = require('rxjs/operators').tap;
+const flatMap  = require('rxjs/operators').flatMap;
 
 const o = Observable.create((subscriber) => {
     subscriber.next('a1'); // fire
@@ -40,3 +45,41 @@ const o = Observable.create((subscriber) => {
 });
 
 o.subscribe(console.log); // a1, d1, g1
+
+// ======================  An example that not works as expected ======================
+// const api = () => Observable.create(subscriber => {
+//     console.log('calling api');
+//     timer(5000).subscribe(() => subscriber.next('emit value.'));
+// });
+//
+// let hasValue  = false;
+// const request = () => Observable.of(1).pipe(
+//     tap(v => hasValue = false),
+//     flatMap(() => api()),
+//     tap(v => hasValue = true),
+// );
+//
+// timer(0, 1000)
+//     .pipe(
+//         throttle(() => {
+//             console.error('calling throttle', hasValue);
+//             return hasValue ? timer(0) : timer(100);
+//         }),
+//     )
+//     .subscribe(() => request().subscribe(console.warn));
+
+// ====================== Instead ======================
+// let calling  = false;
+// const request = () => Observable.of(1).pipe(
+//     flatMap(() => {
+//         if (calling) {
+//             return Observable.never()
+//         }
+//         calling =true;
+//         return api();
+//     }),
+//     tap(v => calling = false),
+// );
+//
+// timer(0, 1000)
+//     .subscribe(() => request().subscribe(console.warn));
