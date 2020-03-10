af17b60974b148be0594fe8af1e8f1cca059c27b
neal.yip <neal.yip@codpayment.com>
2020-03-10 14:52:51 +0800

    Added toArray example

--- flatmap.chaining.js
+++ flatmap.chaining.js
@@ -2,13 +2,15 @@ const Observable = require('rxjs/Observable').Observable;
 require('rxjs/add/observable/of');
 require('rxjs/add/operator/delay');
 require('rxjs/add/operator/mergeMap');
+require('rxjs/add/operator/map');
+const toArray = require('rxjs/operators').toArray;
 
 Observable.of(1)
-.delay(1000)
-.flatMap((value) => {
-    return Observable.of(value + 1)
-})
-.subscribe(console.log); // 2
+    .delay(1000)
+    .flatMap((value) => {
+        return Observable.of(value + 1)
+    })
+    .subscribe(console.log); // 2
 
 Observable.of([1]).flatMap(v => {
     console.log(typeof (v + '234'));
@@ -22,3 +24,7 @@ Observable.of([1]).flatMap(v => {
  3
  4
  **/
+// so use flatmap in conjunction with toArray
+
+Observable.of([100, 101, 102]).flatMap(v => v).map(v => v + 1).pipe(toArray()).subscribe(console.log);
+// [101, 102, 103]
