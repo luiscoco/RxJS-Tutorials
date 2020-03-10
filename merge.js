b56abb2217de6c0f85e1943ac4fa985ae701ff31
neal.yip <neal.yip@codpayment.com>
2020-03-10 16:38:17 +0800

    added example use of merge in conjunction with zip

--- merge.js
+++ merge.js
@@ -3,8 +3,11 @@ const Observable = require('rxjs/Observable').Observable;
 const merge      = require('rxjs/observable/merge').merge;
 const of         = require('rxjs/observable/of').of;
 const never      = require('rxjs/observable/never').never;
-const toArray      = require('rxjs/operators').toArray;
+const toArray    = require('rxjs/operators').toArray;
+const timer    = require('rxjs').timer;
+const zip        = require('rxjs').zip;
 require('rxjs/add/operator/delay');
+require('rxjs/add/operator/map');
 
 merge(of(1), of(2).delay(4000))
     .subscribe(console.log);
@@ -22,3 +25,10 @@ merge(of(3), never())
  */
 
 merge(of(1).delay(400), of(2).delay(900)).pipe(toArray()).subscribe(console.error);
+
+const data = [11, 12];
+
+zip(
+    merge(...data.map(num => timer(900).map(() => `${num}a`))).pipe(toArray()),
+    merge(...data.map(num => timer(900).map(() => `${num}b`))).pipe(toArray()),
+).subscribe(console.error);
