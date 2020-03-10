0f86964e9fe809c62ab6daa3abe4c637c4fb14b0
neal.yip <neal.yip@codpayment.com>
2020-03-10 16:32:07 +0800

    merge + toArray

--- merge.js
+++ merge.js
@@ -1,8 +1,9 @@
 const Observable = require('rxjs/Observable').Observable;
 // require('rxjs/add/observable/merge');
-const merge = require('rxjs/observable/merge').merge;
-const of = require('rxjs/observable/of').of;
-const never = require('rxjs/observable/never').never;
+const merge      = require('rxjs/observable/merge').merge;
+const of         = require('rxjs/observable/of').of;
+const never      = require('rxjs/observable/never').never;
+const toArray      = require('rxjs/operators').toArray;
 require('rxjs/add/operator/delay');
 
 merge(of(1), of(2).delay(4000))
@@ -20,3 +21,4 @@ merge(of(3), never())
  * 2
  */
 
+merge(of(1).delay(400), of(2).delay(900)).pipe(toArray()).subscribe(console.error);
