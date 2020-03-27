aaea60b6275a13da899fa3208ecc8b1f9cb45066
faiyip pc <faiyip@gmail.com>
2020-03-27 15:52:00 +0800

    added pairwise in conjunction with startwith

--- pairwise.js
+++ pairwise.js
@@ -1,8 +1,9 @@
-const timer    = require('rxjs').timer;
-const map      = require('rxjs/operators').map;
-const take     = require('rxjs/operators').take;
-const tap      = require('rxjs/operators').tap;
-const pairwise = require('rxjs/operators').pairwise;
+const timer     = require('rxjs').timer;
+const map       = require('rxjs/operators').map;
+const take      = require('rxjs/operators').take;
+const tap       = require('rxjs/operators').tap;
+const pairwise  = require('rxjs/operators').pairwise;
+const startWith = require('rxjs/operators').startWith;
 
 // Emit the previous and current values as an array
 
@@ -14,3 +15,7 @@ timer(0, 1000).pipe(pairwise(), take(5)).subscribe(console.log);
  [ 3, 4 ]
  [ 4, 5 ]
  */
+
+timer(0, 1000).pipe(startWith(null), pairwise(), take(5)).subscribe(console.warn);
+// [ null, 0 ]
+// [ 0, 1 ]...
