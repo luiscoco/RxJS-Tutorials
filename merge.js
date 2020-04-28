621601261b284ce244173a13ada4040268b56031
neal.yip <neal.yip@codpayment.com>
2020-04-28 12:25:53 +0800

    Added description

--- merge.js
+++ merge.js
@@ -1,16 +1,34 @@
-const Observable = require('rxjs/Observable').Observable;
+const Observable    = require('rxjs/Observable').Observable;
 // require('rxjs/add/observable/merge');
-const merge      = require('rxjs/observable/merge').merge;
-const of         = require('rxjs/observable/of').of;
-const never      = require('rxjs/observable/never').never;
-const toArray    = require('rxjs/operators').toArray;
-const timer    = require('rxjs').timer;
-const zip        = require('rxjs').zip;
+const merge         = require('rxjs/observable/merge').merge;
+const of            = require('rxjs/observable/of').of;
+const never         = require('rxjs/observable/never').never;
+const toArray       = require('rxjs/operators').toArray;
+const timer         = require('rxjs').timer;
+const zip           = require('rxjs').zip;
+const combineLatest = require('rxjs/observable/combineLatest').combineLatest;
 require('rxjs/add/operator/delay');
 require('rxjs/add/operator/map');
 
+
+// merge
+// a   c
+//   b
+// => a b c
+
+// combinelatest
+// a   c
+//   b
+// => [a, b]
+
 merge(of(1), of(2).delay(4000))
     .subscribe(console.log);
+// 1
+// 2
+
+// vs combineLatest
+combineLatest([of(3), of(4).delay(4000)])
+    .subscribe(console.log); // [3, 4]
 
 
 merge(of(3), never())
@@ -32,3 +50,4 @@ zip(
     merge(...data.map(num => timer(900).map(() => `${num}a`))).pipe(toArray()),
     merge(...data.map(num => timer(900).map(() => `${num}b`))).pipe(toArray()),
 ).subscribe(console.error);
+// [ [ '11a', '12a' ], [ '11b', '12b' ] ]
