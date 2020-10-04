a69c0f973a496320960aa228eebd46a08777e1b7
faiyip pc <faiyip@gmail.com>
2020-10-04 16:17:24 +0800

    Added one example

--- takeUntil.js
+++ takeUntil.js
@@ -4,6 +4,7 @@ const first     = require('rxjs/operators').first;
 const tap       = require('rxjs/operators').tap;
 const take      = require('rxjs/operators').take;
 const takeUntil = require('rxjs/operators').takeUntil;
+const Subject = require('rxjs/Subject').Subject;
 
 const waitingSthToHappen = timer(6000); // in this example, it's in 6 seconds.
 // in practice, it can be a button pressed by a user. A tear down of an object and component.
@@ -12,4 +13,13 @@ const waitingSthToHappen = timer(6000); // in this example, it's in 6 seconds.
 timer(0, 1000).pipe(
     takeUntil(waitingSthToHappen),
     tap(console.log)
+).subscribe();
+
+// It doesn't necessarily need to have completed.
+const subject$ = new Subject();
+timer(3000).pipe(tap(() => subject$.next())).subscribe()
+
+timer(0, 1000).pipe(
+    takeUntil(subject$),
+    tap(r => console.warn(r))
 ).subscribe();
