fa3530bc30fe4fca688a5c8d79801a749cc1ac05
faiyip pc <faiyip@gmail.com>
2021-01-27 23:16:21 +0800

    takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -4,6 +4,8 @@ const first     = require('rxjs/operators').first;
 const tap       = require('rxjs/operators').tap;
 const take      = require('rxjs/operators').take;
 const takeUntil = require('rxjs/operators').takeUntil;
+const catchError = require('rxjs/operators').catchError;
+const from = require('rxjs').from;
 const Subject = require('rxjs/Subject').Subject;
 
 const waitingSthToHappen = timer(6000); // in this example, it's in 6 seconds.
@@ -23,3 +25,23 @@ timer(0, 1000).pipe(
     takeUntil(subject$),
     tap(r => console.warn(r))
 ).subscribe();
+
+const subject$ = new Subject();
+timer(3000).pipe(tap(() => {
+    subject$.error('stop a');
+})).subscribe();
+timer(0, 6000).pipe(
+    takeUntil(subject$),
+    takeUntil(from(new Promise(((resolve, reject) => {
+        setTimeout(() => reject('error a'), 3000);
+    })))),
+    // catchError(e => {
+    //     console.error(e);
+    //     return [null];
+    // }),
+    tap(console.log)
+).subscribe(()=>{}, e => console.error('error', e))
+/**
+ 0
+ error stop a
+**/
